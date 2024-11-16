87dfb9e7453b7b15b8fe2b881b734d35d9cdfe74
Samuel LaBrant <sjlabrant@gmail.com>
2024-11-16 11:30:03 -0800

    Fix for mirror rotation.

--- scenes/objects/mirrors/mirror_2_way.gd
+++ scenes/objects/mirrors/mirror_2_way.gd
@@ -1,14 +1,5 @@
 extends "res://scenes/objects/mirrors/mirror.gd"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func duplicate_body(body: Node2D) -> void:
 	var new_body = body.duplicate(DUPLICATE_SIGNALS | DUPLICATE_GROUPS)
